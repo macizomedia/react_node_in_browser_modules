148be680957879bd0552647b3b89a2cd450eca86
macizomedia <info@macizomedia.com>
2021-05-03 23:22:58 +0200

    Update component class to functional & change CSS Framework

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-console.log(datetimepickerFactory)
-jQuery.datetimepicker.setLocale('en');
+const { useState, useEffect, useContext, createContext } = React
 const settings = {
     theme: {
 
@@ -17,23 +16,24 @@ const settings = {
     { "title": "Alert", "id": 87, "priority": "24", "due": "in 8 hours", "isDone": false, "timer": "April 26th 2021, 4:06:25 pm" },
     { "title": "Penada", "id": 98, "priority": "24", "due": "in 8 hours", "isDone": false, "timer": "April 26th 2021, 4:06:25 pm" }]
 };
-const SettingsContext = React.createContext(settings)
+const SettingsContext = createContext(settings)
 const root = document.getElementById('root')
 
-const DatePick = () => {
-    jQuery('#datetimepicker').datepicker({
-                format: 'Y-m-d H:i',
-                inline: true
-            });
-    // const [startDate, setStartDate] = React.useState(new Date().toISOString());
+const DatePick = ({ onClick }) => {
     return (
-        <input id="datetimepicker" className="xdsoft_datetimepicker" type="text"/>
+        <div className="btn-group">
+            <button type="button" className="btn btn-dark" onClick={onClick} value="1">1 Hour</button>
+            <button type="button" className="btn btn-dark" onClick={onClick} value="2" >2 Hours</button>
+            <button type="button" className="btn btn-dark" onClick={onClick} value="6" >6 Hours</button>
+            <button type="button" className="btn btn-dark" onClick={onClick} value="8" >8 Hours</button>
+            <button type="button" className="btn btn-dark" onClick={onClick} value="24" >1 day</button>
+        </div>
     );
 };
 
 const Btn = ({ id, type, icon, name, isActive, onClick }) => (
     <button
-        className={`btn btn-outline-${isActive ? 'primary' : 'secondary'}`}
+        className={`btn-${isActive ? 'primary' : 'secondary'}`}
         onClick={onClick}
         type={type}
         name={name}
@@ -51,7 +51,7 @@ Btn.defaultProps = {
 const Title = ({ title, state }) => {
 
     return (
-        <div className="mt-2">
+        <div className="m-2">
             <h1>Hello, {title} </h1>
             <p>Current {state}</p>
         </div>
@@ -90,13 +90,30 @@ function ActionLink({ id, name, icon, fn }) {
 /* Navegation is wrapped on LogingControl component*/
 function Navigation({ button }) {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">
-                    Task Manager
-                </a>
-                {button}
-            </nav>
+        <div className="header header-fixed unselectable header-animated">
+            <div className="header-brand">
+                <div className="nav-item no-hover">
+                    <h6 className="title">My App</h6>
+                    {button}
+                </div>
+                <div className="nav-item nav-btn" id="header-btn"> <span></span> <span></span> <span></span> </div>
+            </div>
+            <div className="header-nav" id="header-menu">
+                <div className="nav-left">
+                    <div className="nav-item text-center"> <a href="#"> <span className="icon"> <i className="fab fa-wrapper fa-twitter"
+                        aria-hidden="true"></i> </span> </a> </div>
+                </div>
+                <div className="nav-right">
+                    <div className="nav-item has-sub toggle-hover" id="dropdown">
+                        <a className="nav-dropdown-link">none</a>
+                        <ul className="dropdown-menu dropdown-animated" role="menu">
+                            <li role="menu-item"><a href="#">First Item</a></li>
+                            <li role="menu-item"><a href="#">Second Item</a></li>
+                            <li role="menu-item"><a href="#">Third Item</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -111,17 +128,17 @@ const setUser = (val) => {
 }
 
 function GuestGreeting({ user, state }) {
-    const [input, setInput] = React.useState(user)
+    const [input, setInput] = useState(user)
     return (
-        <div className="container-flex m-3 p-2 bg-dark">
-            <Title className="display-5" title={input} state={state} />
-            <div className="row justify-content-center">
+        <div className="card m-3 p-3">
+            <Title className="title" title={input} state={state} />
+            <div className="u-flex u-flex-column">
                 <label htmlFor="user"></label>
                 <input
                     onChange={(e) => setInput(e.target.value)}
                     value={input.name}
                     type="text"
-                    className="col-8 form-control m-2"
+                    className="px-2 py-1 m-1 u-round"
                     name="name"
                     id="user"
                     aria-describedby="helpId"
@@ -130,7 +147,7 @@ function GuestGreeting({ user, state }) {
                 <button
                     onClick={() => setUser(input)} // We safe the user to the LocalStorage!
                     type="button"
-                    className="col-4 btn btn-secondary"
+                    className="px-2 py-1 m-1 u-round btn-secondary"
                 >
                     Set
                 </button>
@@ -148,9 +165,9 @@ function Greeting({ user, isLoggedIn }) {
     /* Greeting is a nested component we want to set the user on our app context */
     if (isLoggedIn) {
         return (
-            <div className="jumbotron">
-                <h1 className="display-5">{`Hello, ${user}`}</h1>
-                <p className="lead">
+            <div className="card m-3 p-4">
+                <h1 className="title">{`Hello, ${user}`}</h1>
+                <p className="subtitle">
                     {settings.name}
                 </p>
                 <hr className="my-4" />
@@ -158,17 +175,9 @@ function Greeting({ user, isLoggedIn }) {
                     It uses utility classes for typography and spacing to space
                     content out within the larger container.
                 </p>
-                <p className="lead">
+                <p className="subtitle">
                     <Btn isActive={isLoggedIn} icon="user" name="set" onClick={null} />
                 </p>
-                {/* <h5 className="display-5">Previous User </h5>
-                <div>New user is {settings}</div>
-                <button
-                    className="btn btn-outline-danger"
-                    onClick={() => setSettings(props.user)}
-                >
-                    Set Context
-                </button> */}
             </div>
         )
     }
@@ -176,54 +185,30 @@ function Greeting({ user, isLoggedIn }) {
 }
 /* TASK COMPONENTS */
 const Task = ({ title, isDone, priority, timer, id, due, onClick, onEdit }) => {
-    if (!isDone) {
-        return (
-            <div id={id} className="list-group">
-                <a href={`${id}`} className="list-group-item list-group-item-action flex-column align-items-start">
+    return (
+        <div id={id} className="list-group">
+            <a href={`${id}`} className="list-group-item list-group-item-action flex-column align-items-start">
 
-                    <div className="card text-white bg-secondary mt-3">
-                        <div className="card-header col-10 ">
-                            <h4 id={`${id}-text`} className={` text-${priority < 50 ? 'warning' : 'info'} text-${isDone ? 'primary' : 'secondary'
-                                }`}>{title}</h4>
-                        </div>
-                        <div className="card-body w-100 p-2 m-3">
-                            <small className=""> created at {timer}</small>
-                            <p className="mb-1">{id}</p>
-                            <small>{due}</small>
-                        </div>
-                        <div className="card-footer bg-warning d-flex">
-                            <Btn className="col-2 text-info" name='edit' icon='edit' onClick={onEdit} />
-                            <Btn className="col-2 card-footer w-25" name='bin' icon='trash' onClick={onClick} />
-                        </div>
-
+                <div className="card text-white bg-secondary mt-3">
+                    <div className="card-header col-10 ">
+                        <h4 id={`${id}-text`} className={` text-${priority < 50 ? 'warning' : 'info'} text-${isDone ? 'primary' : 'secondary'
+                            }`}>{title}</h4>
                     </div>
-                </a>
-            </div>
-        )
-    } else {
-        return (
-
-            <div className="modal">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Modal title</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Open">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p>Modal body text goes here.</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
+                    <div className="card-body w-100 p-2 m-3">
+                        <small className=""> created at {timer}</small>
+                        <p className="mb-1">{id}</p>
+                        <small>{due}</small>
                     </div>
+                    <div className="card-footer bg-warning d-flex">
+                        <Btn className="col-2 text-info" name='edit' icon='edit' onClick={onEdit} />
+                        <Btn className="col-2 card-footer w-25" name='bin' icon='trash' onClick={onClick} />
+                    </div>
+
                 </div>
-            </div>
-        )
-    }
+            </a>
+        </div>
+    )
+
 }
 const TaskListing = ({ tasks }) => {
 
@@ -268,16 +253,16 @@ const TaskListing = ({ tasks }) => {
 
 const InputElem = ({ label, type, id, help, onChange, value }) => {
     return (
-        <div className="p-3">
+        <div className="form-group-input">
             <label htmlFor={id}>{label}</label>
             <input
                 onChange={onChange}
                 type={type}
-                className="form-control"
+                className=""
                 id={id}
                 value={value}
                 aria-describedby="emailHelp"
-                placeholder={`enter ${id}`}
+                placeholder="Enter Task"
             />
             <small id="emailHelp" className="form-text text-muted">
                 {help}
@@ -390,68 +375,62 @@ const fetchTaskList = (fn) => {
         })
 }
 /* CREATE TASK  */
-class CreateTask extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleClick = this.handleClick.bind(this)
-        this.state = {
-            title: '',
-            id: 3,
-            priority: 0,
-            due: '',
-            isDone: false,
-            timer: moment().format('MMMM Do YYYY, h:mm:ss a'),
-            onClick: (e) => console.log(e)
-        }
-    }
-    handleClick(e) {
-        this.setState({ title: e.target.value })
-        e.target.value = ''
-    }
-    handleSubmit = (event) => {
+const CreateTask = () => {
+    const [task, setTask] = useState({
+
+    });
+
+    const handleSubmit = (event) => {
         event.preventDefault()
+        const form = window.document.getElementById('taskFrom')
         //alert(`A task was added: ${JSON.stringify(this.state)}`)
-        saveTask(this.state)
-        this.setState()
+        console.log(task)
+        saveTask(task)
+        setTask({})
     }
-    render() {
-        return (
+
+    return (
+        <div className="card m-3 p-4">
             <form
-                className="form-group p-3"
-                onSubmit={(event) => this.handleSubmit(event)}
+                id='taskForm'
+                className="form-group u-flex-column"
+                onSubmit={(event) => handleSubmit(event)}
             >
                 <InputElem
                     label="enter task"
                     type="text"
                     id="taskInput"
                     help="What are we doing?"
-                    value={this.state.title}
                     onChange={(e) =>
-                        this.setState({
+                        setTask({
+                            ...task,
                             title: e.target.value,
                             id: Math.floor(Math.random() * 100),
                         })
                     }
                 />
-                <DatePick />
                 <InputElem
                     label="Priority"
                     type="range"
                     id="customRange1"
                     help="when are we doing it?"
-                    value={this.state.priority}
                     onChange={(e) =>
-                        this.setState({
-                            priority: e.target.value,
-                            due: moment().endOf('day').fromNow(),
+                        setTask({
+                            ...task, priority: e.target.value,
+                            isDone: false,
+                            timer: moment().format('MMMM Do YYYY, h:mm:ss a'),
                         })
                     }
                 />
-                <input className="btn btn-outline-primary btn-lg btn-block" type="submit" value="Submit" />
+                <DatePick onClick={(e) => setTask({ ...task, due: moment().add(e.target.value, 'hour').calendar() })} />
+                <input className="form-group-btn" type="submit" value="Submit" />
             </form>
-        )
-    }
+        </div>
+
+
+    )
 }
+
 /* AUTH CONTROL */
 /* Login Component is wrap inside App */
 class LoginControl extends React.Component {
